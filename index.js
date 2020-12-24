fdae731c7a1628c872223fb7fd0fa252220e8f6a
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2020-12-24 23:06:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('tejas').onmouseover = function changeContent() {
+
+    document.getElementById('tejas').innerHTML = "Welcome to Divine Destinations";
+    document.getElementById('tejas').style = "Color:#293036";
+
+}
